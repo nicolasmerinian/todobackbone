ca94741607e30b7eb585031fb985ec43f04571a9
Nicolas <nicolas.merinian@gmail.com>
2016-10-31 19:14:20 +0100

    create todoview

---
+++ scripts/views/TodoView.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let Backbone = require('Backbone');
+
+var TodoView = Backbone.Collection.extend({
+
+	tagName: 'li',
+
+	initialize: function() {
+
+	},
+
+	render: function() {
+		
+	}
+
+});
+
+module.exports = TodoView;
